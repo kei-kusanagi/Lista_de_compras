9988319c51691248c4dbc77cc2cb451747e92793
kei-kusanagi <kei_kusanagi@outlook.com>
2023-05-24 19:24:17 -0600

    add list

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -8,11 +8,88 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final _items = [];
+  final GlobalKey<AnimatedListState> _key = GlobalKey();
+  int _selectedIndex = 0;
+
+  void _addItem() {
+    _items.insert(0, "Item ${_items.length + 1}");
+    _key.currentState!.insertItem(
+      0,
+      duration: const Duration(seconds: 1),
+    );
+  }
+
   @override
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+
+    if (index == 0) {
+      _addItem();
+      // crear una nueva lista
+    } else if (index == 1) {
+      // boton para buscar por dia
+    }
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Lista de Compras'),
+      ),
+      body: Column(
+        children: [
+          const SizedBox(height: 10),
+          IconButton(
+            onPressed: _addItem,
+            icon: const Icon(Icons.add),
+          ),
+          Expanded(
+            child: AnimatedList(
+              key: _key,
+              initialItemCount: 0,
+              padding: const EdgeInsets.all(10),
+              itemBuilder: (context, index, animation) {
+                return SizeTransition(
+                  key: UniqueKey(),
+                  sizeFactor: animation,
+                  child: Card(
+                    margin: const EdgeInsets.all(10),
+                    color: Colors.orangeAccent,
+                    child: ListTile(
+                      title: Text(
+                        _items[index],
+                        style: const TextStyle(fontSize: 24),
+                      ),
+                      trailing: IconButton(
+                        icon: const Icon(Icons.delete),
+                        onPressed: () {},
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          )
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add),
+            label: 'Nueva Lista',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Buscar',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Theme.of(context).canvasColor,
+        backgroundColor: Theme.of(context).primaryColor,
+        onTap: _onItemTapped,
       ),
     );
   }
